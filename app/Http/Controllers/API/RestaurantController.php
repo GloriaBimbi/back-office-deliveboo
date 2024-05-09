3e84d36794d3d5893e11479c05a0ebc0eaee067c
GiPisano <pisanogiacomo9@gmail.com>
2024-05-09 18:38:30 +0200

    fix show restaurant api

--- app/Http/Controllers/API/RestaurantController.php
+++ app/Http/Controllers/API/RestaurantController.php
@@ -80,7 +80,23 @@ class RestaurantController extends Controller
         $restaurant = Restaurant::select(['id', 'user_id', 'name', 'description', 'address', 'image', 'slug'])
             ->with(['user:id,name,email', 'dishes:id,restaurant_id,name,image,description,price,ingredients_list,slug', 'types:id,name,logo,color'])
             ->where('slug', $slug)
-            ->paginate(10);
+            ->first();
+        // ->paginate(10);
+        foreach ($restaurant->dishes as $dish) {
+            if (!str_starts_with($dish->image, 'https')) {
+
+                $dish->image = !empty($dish->image)
+                    ? $dish->getImage()
+                    : null;
+            }
+        }
+        if (!str_starts_with($restaurant->image, 'https')) {
+
+            $restaurant->image = !empty($restaurant->image)
+                ? $restaurant->getImage()
+                : null;
+        }
+
         if (!$restaurant) {
             return response()->json([
                 'message' => 'Restaurant not found'
