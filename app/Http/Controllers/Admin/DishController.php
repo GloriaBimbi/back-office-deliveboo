675abd5c24bbfbfce4a33293a250c234c2fc9e7a
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-05-09 10:20:29 +0200

    feat: fix slug repetition and dish show navigation link

--- app/Http/Controllers/Admin/DishController.php
+++ app/Http/Controllers/Admin/DishController.php
@@ -55,6 +55,7 @@ class DishController extends Controller
         $current_user = Auth::id();
         $restaurant = Restaurant::where('user_id', $current_user)->first();
         $dish->slug = Str::slug($dish->name);
+        $dish->slug = Dish::generateUniqueSlug($dish->name);
         $restaurant_id = $restaurant->id;
         $dish->restaurant_id = $restaurant_id;
         $dish->visible = true;
