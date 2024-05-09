7d17be0d55d9d3b3e6483c2f6cf6fad6dcc8a305
Christian-Lepidi <christianlepidi@gmail.com>
2024-05-09 11:03:53 +0200

    feat: add apiroutes

---
+++ app/Http/Controllers/API/RestaurantController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     
+     */
+    public function index()
+    {
+        $restaurants = Restaurant::select(['id', 'user_id', 'name', 'address', 'image', 'slug'])->with(['user:id,name,email', 'dishes:id,name,image,description,price,ingredients_list,slug', 'types:id,name,logo,color'])->paginate(10);
+        dd($restaurants);
+        return response()->json($restaurants);
+
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     
+     */
+    public function show($slug)
+    { {
+            $restaurant = Restaurant::with('type', 'user', 'dishes')->where('slug', $slug);
+            if (!$restaurant) {
+                return response()->json([
+                    'message' => 'Restaurant not found'
+                ], 404);
+            }
+            return response()->json($restaurant);
+        }
+    }
+
+}
