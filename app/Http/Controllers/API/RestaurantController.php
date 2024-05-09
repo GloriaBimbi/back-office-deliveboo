c2d5c4f3bc9ee4251f6420aa1ac9fcbdb2a252c4
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-05-09 12:50:12 +0200

    fix image api data

--- app/Http/Controllers/API/RestaurantController.php
+++ app/Http/Controllers/API/RestaurantController.php
@@ -15,11 +15,24 @@ class RestaurantController extends Controller
      */
     public function index()
     {
-        $restaurants = Restaurant::select(['id', 'user_id', 'name','description', 'address', 'image', 'slug'])->with(['user:id,name,email', 'dishes:id,restaurant_id,name,image,description,price,ingredients_list,slug', 'types:id,name,logo,color'])->paginate(10);
+        $restaurants = Restaurant::select(['id', 'user_id', 'name','description', 'address', 'image', 'slug'])
+        ->with(['user:id,name,email', 'dishes:id,restaurant_id,name,image,description,price,ingredients_list,slug', 'types:id,name,logo,color'])
+        ->paginate(10);
+
+        foreach($restaurants as $restaurant){
+            if(!str_starts_with($restaurant->image,'https')){
+
+                $restaurant->image=!empty($restaurant->image)
+                ?$restaurant->getImage()
+                :null;
+            } 
+        }
 
         return response()->json($restaurants);
 
     }
+}
+// }
 
 
 
@@ -29,16 +42,16 @@ class RestaurantController extends Controller
      * @param  int  $id
      
      */
-    public function show($slug)
-    { {
-            $restaurant = Restaurant::with('type', 'user', 'dishes')->where('slug', $slug);
-            if (!$restaurant) {
-                return response()->json([
-                    'message' => 'Restaurant not found'
-                ], 404);
-            }
-            return response()->json($restaurant);
-        }
-    }
+    // public function show($slug)
+    // { {
+    //         $restaurant = Restaurant::with('type', 'user', 'dishes')->where('slug', $slug);
+    //         if (!$restaurant) {
+    //             return response()->json([
+    //                 'message' => 'Restaurant not found'
+    //             ], 404);
+    //         }
+    //         return response()->json($restaurant);
+    //     }
+    // }
 
-}
+// }
