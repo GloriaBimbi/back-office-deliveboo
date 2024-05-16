c6ff6f8543510d75c24cfef136fa63ea56fd55bf
GiPisano <pisanogiacomo9@gmail.com>
2024-05-16 17:34:52 +0200

    fix braintree

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -31,52 +31,52 @@ class PaymentController extends Controller
     {
 
         $nonceFromTheClient = $request->input('paymentMethodNonce');
-        $totalPrice = ltrim($request->input('amount'),'$');
+        $totalPrice = ltrim($request->input('amount'), '$');
 
-        $validated=$request->all();
+        $validated = $request->all();
 
-        $formData=$validated['formData'];
+        $formData = $validated['formData'];
 
         $order = new Order;
-            $order->name = $formData['name'];
-            $order->lastname = $formData['lastname'];
-            $order->address = $formData['addressStreet']. ', '. $formData['addressCivic']. ', '. $formData['addressCap'].''. $formData['addressCity'].'('. $formData['addressCountry']. ')';
-            $order->email = $formData['email'];
-            $order->phone = $formData['phone'];
-            $order->card_token = $validated['paymentMethodNonce'];
-            $order->total_price=$totalPrice;
-            $order->save();
+        $order->name = $formData['name'];
+        $order->lastname = $formData['lastname'];
+        $order->address = $formData['addressStreet'] . ', ' . $formData['addressCivic'] . ', ' . $formData['addressCap'] . '' . $formData['addressCity'] . '(' . $formData['addressCountry'] . ')';
+        $order->email = $formData['email'];
+        $order->phone = $formData['phone'];
+        $order->card_token = $validated['paymentMethodNonce'];
+        $order->total_price = $totalPrice;
+        $order->save();
 
-            foreach($validated['cart'] as $item){
-var_dump($item);
-                $order->dishes()->attach($item['id'],['quantity'=>$item['quantity']]); // Collega i piatti all'ordine
-            }
+        foreach ($validated['cart'] as $item) {
+            // var_dump($item);
+            $order->dishes()->attach($item['id'], ['quantity' => $item['quantity']]); // Collega i piatti all'ordine
+        }
 
-            var_dump($validated);
+        // var_dump($validated);
 
-            // Elaborazione del pagamento con Braintree
-            $gateway = new Gateway([
-                'environment' => env('BRAINTREE_ENV'),
-                'merchantId' => env('BRAINTREE_MERCHANT_ID'),
-                'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
-                'privateKey' => env('BRAINTREE_PRIVATE_KEY'),
-            ]);
-            $result = $gateway->transaction()->sale([
-                'amount' => $totalPrice,
-                'paymentMethodNonce' => $validated['paymentMethodNonce'],
-                // Altri dettagli della transazione
-            ]);
+        // Elaborazione del pagamento con Braintree
+        $gateway = new Gateway([
+            'environment' => env('BRAINTREE_ENV'),
+            'merchantId' => env('BRAINTREE_MERCHANT_ID'),
+            'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
+            'privateKey' => env('BRAINTREE_PRIVATE_KEY'),
+        ]);
+        $result = $gateway->transaction()->sale([
+            'amount' => $totalPrice,
+            'paymentMethodNonce' => $validated['paymentMethodNonce'],
+            // Altri dettagli della transazione
+        ]);
 
-            // Gestione della risposta di Braintree
-            if ($result->success) {
-                // Transazione completata con successo
-                // Registra i dettagli della transazione nel tuo sistema
-                return response()->json(['message' => 'Payment completed successfully']);
-            } else {
-                // Errore durante il pagamento
-                $error = $result->message;
-                return response()->json(['error' => $error], 422);
-            }
+        // Gestione della risposta di Braintree
+        if ($result->success) {
+            // Transazione completata con successo
+            // Registra i dettagli della transazione nel tuo sistema
+            return response()->json(['message' => 'Payment completed successfully']);
+        } else {
+            // Errore durante il pagamento
+            $error = $result->message;
+            return response()->json(['error' => $error], 422);
+        }
     }
 }
 
