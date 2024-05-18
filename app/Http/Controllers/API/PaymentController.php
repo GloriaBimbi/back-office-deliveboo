d26d4262c2d0c5858bf0eeb4eeced0b2ed574b7f
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-05-18 16:53:44 +0200

    feat: add validation to payment form data

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -29,15 +29,27 @@ class PaymentController extends Controller
         $nonceFromTheClient = $request->input('paymentMethodNonce');
         $totalPrice = ltrim($request->input('amount'), '$');
 
-        $validated = $request->all();
-        $formData = $validated['formData'];
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'lastname' => 'required|string|max:255',
+
+            'address_street' => 'required|string|max:255',
+            'address_civic' => 'required|string|max:10',
+            'address_postal_code' => 'required|string|max:5|min:5',
+            'address_city' => 'required|string|max:100',
+            'address_country' => 'required|string|max:100',
+
+            'email' => 'required|email|max:255',
+            'phone' => 'required|string|max:10|min:9',
+
+        ]);
 
         $order = new Order;
-        $order->name = $formData['name'];
-        $order->lastname = $formData['lastname'];
-        $order->address = $formData['addressStreet'] . ', ' . $formData['addressCivic'] . ', ' . $formData['addressCap'] . ' ' . $formData['addressCity'] . '(' . $formData['addressCountry'] . ')';
-        $order->email = $formData['email'];
-        $order->phone = $formData['phone'];
+        $order->name = $validated['name'];
+        $order->lastname = $validated['lastname'];
+        $order->address = $validated['addressStreet'] . ', ' . $validated['addressCivic'] . ', ' . $validated['addressCap'] . ' ' . $validated['addressCity'] . '(' . $validated['addressCountry'] . ')';
+        $order->email = $validated['email'];
+        $order->phone = $validated['phone'];
         $order->card_token = $validated['paymentMethodNonce'];
         $order->total_price = $totalPrice;
         $order->save();
@@ -86,17 +98,3 @@ class PaymentController extends Controller
 }
 
 
-        // $validated = $request->validate([
-        //     'name' => 'required|string|max:255',
-        //     'lastname' => 'required|string|max:255',
-
-        //     'address_street' => 'required|string|max:255',
-        //     'address_civic' => 'required|string|max:10',
-        //     'address_postal_code' => 'required|string|max:5|min:5',
-        //     'address_city' => 'required|string|max:100',
-        //     'address_country' => 'required|string|max:100',
-
-        //     'email' => 'required|email|max:255',
-        //     'phone' => 'required|string|max:10|min:9',
-
-        // ]);
