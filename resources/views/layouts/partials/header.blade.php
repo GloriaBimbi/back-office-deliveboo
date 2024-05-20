54d9fd07b89d04fd307df48c6b8b3d4f3265f6e3
GiPisano <pisanogiacomo9@gmail.com>
2024-05-20 17:29:21 +0200

    fix header

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,77 +1,56 @@
 <header class="border-bottom">
-    <nav class="navbar navbar-expand-lg ">
-        <div class="container-fluid d-flex justify-content-between align-items-center">
-            <div class="nav-logo-wrapper mx-5">
-                <img src="{{ asset('storage/' . 'Delivebootitle.png') }}" alt="" style="width: 20%" href="#">
-            </div>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('storage/' . 'Delivebootitle.png') }}" alt="" style="width: 64px" href="#">
+            </a>
+            <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse text-end" id="navbarNavDropdown">
+                <ul class="navbar-nav ms-auto d-flex gap-2">
+                    @auth
+                        <li class="nav-item my-2">
+                            <a class="button" @class(['nav-link', 'active' => Route::currentRouteName() == 'home']) aria-current="page"
+                                href="{{ route('home') }}">Home</a>
+                        </li>
+                    @endauth
 
-            <div class="nav-right">
-                <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
-                    class="navbar-toggler " data-bs-target="#navbarSupportedContent" data-bs-toggle="collapse"
-                    type="button">
-                    <span class="navbar-toggler-icon text-white"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                    <ul class="navbar-nav my-2 mb-lg-0  d-flex gap-2 align-items-center">
-                        {{-- key home restaurants dishes --}}
-                        @auth
-                            <li class="nav-item">
-                                <a class=" home-btn" @class(['nav-link', 'active' => Route::currentRouteName() == 'home']) aria-current="page"
-                                    href="{{ route('home') }}">Home</a>
-                            </li>
-                        @endauth
-
-                        {{-- @auth
-                            <li class="nav-item">
-                                <a class="nav-link badge bordered p-3 " @class([
-                                    'nav-link',
-                                    'active' => Route::currentRouteName() == 'admin.restaurants.index',
-                                ]) aria-current="page"
-                                    href="{{ route('admin.restaurants.index') }}">Restaurants</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link badge bordered p-3" @class([
-                                    'nav-link',
-                                    'active' => Route::currentRouteName() == 'admin.dishes.index',
-                                ]) aria-current="page"
-                                    href="{{ route('admin.dishes.index') }}">Dishes</a>
-                            </li>
-                        @endauth --}}
-                        @guest
-                            <li class="nav-item">
-                                <a class=" home-btn" class="nav-link" aria-current="page"
-                                    href="http://localhost:5173/">Order</a>
-                            </li>
+                    @guest
+                        <li class="nav-item">
+                            <a class="" class="nav-link" aria-current="page" href="http://localhost:5173/">Order</a>
+                        </li>
+                    </ul>
+                @else
+                    {{-- dropdown list validated  --}}
+                    {{-- <ul class="list-group list-group-flush"> --}}
+                    {{-- dropdown text --}}
+                    <li class="nav-item dropdown text-end button py-0 ms-auto">
+                        <a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle ms-2 my-2 text-white"
+                            data-bs-toggle="dropdown" href="#" id="navbarDropdown" role="button">
+                            <i class="fa-solid fa-user text-white"></i>
+                            {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item " href="{{ route('admin.dashboard') }}">
+                                    Dashboard</a></li>
+                            <li><a class="dropdown-item" href="{{ url('profile') }}"> Profile</a></li>
+                            <li><a class="dropdown-item" href="{{ route('logout') }}" id="logout-link">
+                                    Logout
+                                </a></li>
                         </ul>
-                    @else
-                        {{-- dropdown list validated  --}}
-                        <ul class="list-group list-group-flush">
-                            {{-- dropdown text --}}
-                            <li class="ms-2  dropdown text-end list-group-item homedropdown dropdown-button dropdown-btn">
-                                <a aria-expanded="false" aria-haspopup="true" class=" dropdown-toggle "
-                                    data-bs-toggle="dropdown" href="#" id="navbarDropdown" role="button">
-                                    <i class="fa-solid fa-user me-2 text-white"></i>
-                                    {{ Auth::user()->name }}
-                                </a>
-                                {{-- dropdown  --}}
-                                <div aria-labelledby="navbarDropdown" class="dropdown-menu dropdown-menu-end  homedropdown">
-                                    <a class="dropdown-item homedropdown hovered" href="{{ route('admin.dashboard') }}">
-                                        Dashboard</a>
-                                    <a class="dropdown-item homedropdown hovered" href="{{ url('profile') }}"> Profile</a>
-                                    <a class="dropdown-item homedropdown hovered" href="{{ route('logout') }}"
-                                        id="logout-link">
-                                        Logout
-                                    </a>
 
-                                    <form action="{{ route('logout') }}" class="d-none" id="logout-form" method="POST">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        </ul>
-                    @endguest
-                </div>
+                        {{-- dropdown  --}}
+                        <div aria-labelledby="navbarDropdown" class="dropdown-menu dropdown-menu-end  homedropdown">
+                            <form action="{{ route('logout') }}" class="d-none" id="logout-form" method="POST">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                    {{-- </ul> --}}
+                @endguest
+                </ul>
             </div>
         </div>
     </nav>
